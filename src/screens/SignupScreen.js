6b57c97ad4001352c50f12d13048d6b49549f961
Kevin <kevinyckim33@gmail.com>
2021-03-23 17:41:23 -0400

    Sign up styling almost done

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,22 +1,42 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+// https://reactnativeelements.com/docs/input
+import { Text, Input, Button } from "react-native-elements"; // basically Bootstrap for RN
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
-      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-      <Button
-        title="Go to Signin"
-        onPress={() => navigation.navigate("Signin")}
-      />
-      <Button
-        title="Go to main flow"
-        onPress={() => navigation.navigate("mainFlow")}
-      />
-    </>
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+// navigation v5
+// https://reactnavigation.org/blog/2020/02/06/react-navigation-5.0/#update-options-from-component
+SignupScreen.navigationOptions = () => {
+  return { headerShown: false }; // hides the header
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // borderWidth: 10,
+    // borderColor: "red",
+    flex: 1, // it's like the View is inside a big flex container that is the screen; stretch down to fill the screen;
+    // flexDirection: row; flex: 1 ==> stretch to the right to fill up remaining horizontal space
+    // flexDirection: column; flex: 1 ==> stretch down to fill up remaning vertical space
+    justifyContent: "center", // flexDirection: row; justifyContent ==> center horizontally
+    // flexDirection: column; justifyContent: center ==> center vertically
+    marginBottom: 250, // pretty good; the centering was too awkward
+  },
+});
 
 export default SignupScreen;
